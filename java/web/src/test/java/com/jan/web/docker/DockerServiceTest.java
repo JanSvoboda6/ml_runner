fbc922e79b81a48895808b825330c59adac9e5a8
JanSvoboda6 <jansvoboda596@gmail.com>
2022-02-12 13:10:57 +0100

    Formatting and refactoring DockerServiceTest

--- java/web/src/test/java/com/jan/web/docker/DockerServiceTest.java
+++ java/web/src/test/java/com/jan/web/docker/DockerServiceTest.java
@@ -1,5 +1,6 @@
 package com.jan.web.docker;
 
+import org.assertj.core.api.Assertions;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
@@ -29,6 +30,8 @@ class DockerServiceTest
     {
         DefaultDockerClientConfig.Builder config = DefaultDockerClientConfig.createDefaultConfigBuilder();
         dockerClient = DockerClientBuilder.getInstance(config.build()).build();
+        validateThatDockerIsRunning();
+
         containerRepository = Mockito.mock(ContainerRepository.class);
         userRepository = Mockito.mock(UserRepository.class);
         dockerService = new DockerService(containerRepository, userRepository);
@@ -41,6 +44,7 @@ class DockerServiceTest
         User user = new User(USER_ID, "user@domain.com", "password");
         Mockito.when(userRepository.getById(USER_ID)).thenReturn(user);
         Mockito.when(containerRepository.save(Mockito.any())).thenReturn(new ContainerEntity(HOST_PORT, user));
+
         dockerService.buildDockerContainer(USER_ID);
         List<Container> containers = dockerClient.listContainersCmd().withShowAll(true).exec();
 
@@ -57,12 +61,12 @@ class DockerServiceTest
                 }
             }
 
-            if(isContainerCreated)
+            if (isContainerCreated)
             {
                 break;
             }
         }
-        Assertions.assertTrue(isContainerCreated);
+        Assertions.assertThat(isContainerCreated).isTrue();
     }
 
     @Test
@@ -92,15 +96,22 @@ class DockerServiceTest
                     break;
                 }
             }
-            if(idOfContainerCreatedInTest.length() > 0)
+            if (idOfContainerCreatedInTest.length() > 0)
             {
                 break;
             }
         }
 
-        if(idOfContainerCreatedInTest.length() > 0)
+        if (idOfContainerCreatedInTest.length() > 0)
         {
             dockerClient.removeContainerCmd(idOfContainerCreatedInTest).withForce(true).exec();
         }
+    }
+
+    private void validateThatDockerIsRunning()
+    {
+        Assertions.assertThatCode(() -> dockerClient.pingCmd().exec())
+                .withFailMessage("Docker services cannot be reached. Make sure that Docker engine is running.")
+                .doesNotThrowAnyException();
     }
 }
