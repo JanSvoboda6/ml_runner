2e5c4f072d6c5c67302c7caa4b5dd25696da1dc6
JanSvoboda6 <jansvoboda596@gmail.com>
2022-02-14 13:58:52 +0100

    Test cases for project API added to ApiTest

--- java/web/src/test/java/com/jan/web/api/ApiTest.java
+++ java/web/src/test/java/com/jan/web/api/ApiTest.java
@@ -1,5 +1,6 @@
 package com.jan.web.api;
 
+import com.jan.web.project.ProjectRepository;
 import com.jan.web.security.role.Role;
 import com.jan.web.security.role.RoleRepository;
 import com.jan.web.security.role.RoleType;
@@ -8,6 +9,10 @@ import com.jan.web.security.user.UserCreator;
 import com.jan.web.security.user.UserRepository;
 import com.jan.web.security.utility.JsonWebTokenUtility;
 import org.assertj.core.api.Assertions;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,12 +23,14 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -58,6 +65,9 @@ public class ApiTest
     @Autowired
     private JsonWebTokenUtility jsonWebTokenUtility;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     private User user;
     private String jwtToken;
 
@@ -78,6 +88,7 @@ public class ApiTest
     @AfterEach
     public void after()
     {
+        projectRepository.deleteAll(projectRepository.findAllByUser(user));
         userRepository.delete(user);
     }
 
@@ -100,4 +111,51 @@ public class ApiTest
                         .getBody())
                 .contains("User Content.");
     }
+
+    @Test
+    public void whenUserTriesToCreateProject_thenProjectIsCreated() throws JSONException
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwtToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject json = new JSONObject();
+        json.put("projectName", "random name");
+        HttpEntity<String> request = new HttpEntity<>(json.toString(), headers);
+
+        restTemplate.exchange(BASE_URL + port + "/api/project/saveproject", HttpMethod.POST, request, String.class);
+
+        Assertions.assertThat(projectRepository.findAllByUser(user)).isNotNull();
+    }
+
+    @Test
+    public void whenUserHasNoProjectsCreated_thenNoProjectIsReturned()
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwtToken);
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        Assertions.assertThat(restTemplate.exchange(BASE_URL + port + "/api/project", HttpMethod.GET, request, String.class)
+                        .getBody())
+                .contains("");
+    }
+
+    @Test
+    public void whenUserHasCreatedProjects_thenProjectsAreReturned() throws JSONException, IOException
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwtToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject json = new JSONObject();
+        json.put("projectName", "random name");
+        HttpEntity<String> request = new HttpEntity<>(json.toString(), headers);
+
+
+        restTemplate.exchange(BASE_URL + port + "/api/project/saveproject", HttpMethod.POST, request, String.class);
+        String body = restTemplate.exchange(BASE_URL + port + "/api/project", HttpMethod.GET, request, String.class).getBody();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonResponse = objectMapper.readTree(body);
+        Assertions.assertThat(jsonResponse.get(0).get("name").toString()).contains("random name");
+    }
+
 }
