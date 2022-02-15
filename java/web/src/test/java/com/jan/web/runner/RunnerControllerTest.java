e3e66c75fea77688da242744d5ef26e7ac9ff93b
JanSvoboda6 <jansvoboda596@gmail.com>
2022-02-15 09:39:45 +0100

    Test cases added to RunnerControllerTest

--- java/web/src/test/java/com/jan/web/runner/RunnerControllerTest.java
+++ java/web/src/test/java/com/jan/web/runner/RunnerControllerTest.java
@@ -1,6 +1,61 @@
 package com.jan.web.runner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RunnerControllerTest
 {
+    @Test
+    public void whenRequestForRunningProject_thenRunnerIsPersisted()
+    {
+        Assertions.fail("Test case not implemented");
+    }
 
+    @Test
+    public void whenRequestForResultForFinishedRunnerForFirstTime_thenResultIsObtainedFromContainer()
+    {
+        Assertions.fail("Test case not implemented");
+    }
+
+    @Test
+    public void whenRequestForResultForFinishedRunnerForFirstTime_thenResultIsPersisted()
+    {
+        Assertions.fail("Test case not implemented");
+    }
+
+    @Test
+    public void whenRequestForResultForFinishedRunnerForSecondTime_thenResultIsObtainedFromLocalSource()
+    {
+        Assertions.fail("Test case not implemented");
+    }
+
+    @Test
+    public void whenRequestForResultForUnfinishedRunner_thenMessageInReturned()
+    {
+        Assertions.fail("Test case not implemented");
+    }
+
+    @Test
+    public void whenRequestForFinished_thenInformationIsObtainedFromContainer()
+    {
+        Assertions.fail("Test case not implemented");
+    }
+
+    @Test
+    public void whenRequestForFinishedForAlreadyFinishedRunner_thenInformationIsObtainedFromLocalSource()
+    {
+        Assertions.fail("Test case not implemented");
+    }
+
+    @Test
+    public void whenRunnerIsFinished_thenFinishedInformationIsPersisted()
+    {
+        Assertions.fail("Test case not implemented");
+    }
+
+    @Test
+    public void whenRunnerIsNotFinished_thenNotFinishedInformationIsPersisted()
+    {
+        Assertions.fail("Test case not implemented");
+    }
 }
