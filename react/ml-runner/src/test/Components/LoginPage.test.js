2f207b75c7019056c5040ca3ba0703f7b63d175c
JanSvoboda6 <jansvoboda596@gmail.com>
2022-02-22 10:45:12 +0100

    Implementation of test cases and fixing the act() wrapping in LoginPage.test

--- react/ml-runner/src/test/Components/LoginPage.test.js
+++ react/ml-runner/src/test/Components/LoginPage.test.js
@@ -7,6 +7,8 @@ import {createMemoryHistory} from 'history';
 import LoginService from "../../services/LoginService";
 import Login from "../../components/pages/LoginPage";
 import userEvent from "@testing-library/user-event";
+import {act} from 'react-dom/test-utils';
+import {LocalStorageMock} from '@react-mock/localstorage';
 
 describe('Rendering', () => {
     test('When form is rendered then submit button is enabled', () => {
@@ -34,27 +36,63 @@ describe('Login', () => {
         jest.spyOn(redux, 'useDispatch').mockReturnValue(jest.fn());
         const history = createMemoryHistory();
         const user = {
-            username: 'jan@jan.com',
-            password: 'jan',
+            username: 'user@domain.com',
+            password: 'password',
             accessToken: 'a random token'
         }
         jest.spyOn(LoginService, 'login').mockResolvedValue({user: user});
         history.replace = jest.fn();
-        render(<Router history={history}><LoginPage/></Router>);
+
+        await act(async () => {
+            render(<Router history={history}><LoginPage/></Router>);
+        })
 
         const email = screen.getByPlaceholderText(/email/i);
         const password = screen.getByPlaceholderText(/password/i);
-        const loginButton = screen.getByText(/login/i);
+        const loginButton = screen.getByText("Login");
 
-        fireEvent.change(email, {target: {value: 'user@user.com'}});
-        fireEvent.change(password, {target: {value: 'password'}});
-        userEvent.click(loginButton);
-
-        await waitFor(() => expect(loginButton).not.toBeInTheDocument());
+        await act(async () => {
+            fireEvent.change(email, {target: {value: user.username}});
+            fireEvent.change(password, {target: {value: user.password}});
+            userEvent.click(loginButton);
+        });
         expect(history.replace).toHaveBeenCalledWith(expect.objectContaining({"pathname": "/preparing"}));
     });
-    it.todo('When form is not successfully submitted then user is not redirected to the main page');
-    it.todo('When form is successfully submitted then user information are stored in the local storage');
+
+    test('When form is not successfully submitted then user is not redirected to the main page', async () => {
+        jest.spyOn(redux, 'useDispatch').mockReturnValue(jest.fn());
+        const history = createMemoryHistory();
+
+        let message = 'A problem occurred';
+        const error = {
+            response: {
+                data: {
+                    message: message
+                }
+            }
+        }
+
+        jest.spyOn(LoginService, 'login').mockRejectedValue(error);
+        history.replace = jest.fn();
+
+        await act(async () => {
+            render(<Router history={history}><LoginPage/></Router>);
+        });
+
+        const email = screen.getByPlaceholderText(/email/i);
+        const password = screen.getByPlaceholderText(/password/i);
+        const loginButton = screen.getByText("Login");
+
+        await act(async () => {
+            fireEvent.change(email, {target: {value: 'user@user.com'}});
+            fireEvent.change(password, {target: {value: 'password'}});
+            fireEvent.click(loginButton);
+        });
+
+        const errorMessage = screen.getByText(message);
+        expect(errorMessage).toBeInTheDocument();
+    });
+
     it.todo('When form is not successfully submitted then user information are not stored in the local storage');
     it.todo('When form is submitted then POST request is sent');
     it.todo('When form is submitted and there is an exception on server side then error message is displayed');
