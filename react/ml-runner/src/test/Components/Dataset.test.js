f2675a4e3ab02fc6ec6724dfd00d2b5f77761025
JanSvoboda6 <jansvoboda596@gmail.com>
2022-02-26 16:19:53 +0100

    Implementation of Dataset rendering test case in Dataset.test.js

--- react/ml-runner/src/test/Components/Dataset.test.js
+++ react/ml-runner/src/test/Components/Dataset.test.js
@@ -1,9 +1,39 @@
-describe("Dataset manipulation", () => {
-    it.todo("When Dataset is rendered then files are shown");
+import React from 'react';
+import {render, screen} from "@testing-library/react";
+import {act} from 'react-dom/test-utils';
+import DatasetService from "../../components/dataset/DatasetService";
+import Datasets from "../../components/dataset/Datasets";
+
+describe("Dataset rendering", () => {
+    test("When Dataset is rendered then files are shown", async () => {
+        jest.spyOn(DatasetService, "getFiles").mockResolvedValue({
+            'data': [
+                {
+                    'key': 'aRandomFile.txt'
+                },
+                {
+                    'key': 'aRandomDirectory/'
+                }
+            ]
+        });
+
+        await act(async () => {
+            render(
+                <Datasets/>
+            )
+        });
+
+        expect(screen.getByText('aRandomFile.txt')).toBeInTheDocument();
+        expect(screen.getByText('aRandomDirectory')).toBeInTheDocument();
+    });
 })
 
-describe("Creating files and folders", () => {});
-describe("Renaming files and folders", () => {});
-describe("Deleting files and folders", () => {});
-describe("Downloading files and folders", () => {});
+describe("Creating files and folders", () => {
+});
+describe("Renaming files and folders", () => {
+});
+describe("Deleting files and folders", () => {
+});
+describe("Downloading files and folders", () => {
+});
 
