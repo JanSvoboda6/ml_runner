eba7e6c9bcaaaebbeb24010981d095a767100c5e
JanSvoboda6 <jansvoboda596@gmail.com>
2022-03-12 18:20:53 +0100

    Implementation of result handling test suite in Runner.test.js

--- react/ml-runner/src/test/Components/Runner.test.js
+++ react/ml-runner/src/test/Components/Runner.test.js
@@ -71,6 +71,32 @@ describe('Stop handling', () =>
 
 describe('Result handling', () =>
 {
-    it.todo('When runner is successfully finished then results are shown');
+    test('When runner is successfully finished then results are shown', async() => {
+        jest.useFakeTimers();
+        jest.spyOn(global, 'setInterval');
+        const initialResponse = {
+            'data': {
+                'gammaParameter': 1,
+                'cParameter': 1,
+                'finished': true
+            }
+        };
+
+        const resultResponse = {
+            'data':{
+                'firstLabelResult': 0.7,
+                'secondLabelResult': 0.8
+            }
+        };
+
+        jest.spyOn(axios, 'get').mockResolvedValueOnce(initialResponse).mockResolvedValueOnce(resultResponse);
+        await act(async () => {
+            render(<Router history={createMemoryHistory()}><Runner/></Router>);
+        });
+        //TODO: add result numeric values
+        expect(screen.getByText(/validation result of first label: 70.00%/i)).toBeInTheDocument();
+        expect(screen.getByText(/validation result of second label: 80.00%/i)).toBeInTheDocument();
+    });
+
     it.todo('When there is a problem with running the project then error message is shown');
 });
