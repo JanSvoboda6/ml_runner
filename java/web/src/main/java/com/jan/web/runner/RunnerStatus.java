8719c078388b988253552d2d00c9f9eff4d77123
JanSvoboda6 <jansvoboda596@gmail.com>
2022-03-22 08:45:20 +0100

    isEndState added to RunnerStatus

--- java/web/src/main/java/com/jan/web/runner/RunnerStatus.java
+++ java/web/src/main/java/com/jan/web/runner/RunnerStatus.java
@@ -2,10 +2,23 @@ package com.jan.web.runner;
 
 public enum RunnerStatus
 {
-    PREPARING_DATA,
-    TRAINING,
-    PREDICTING,
-    FINISHED,
-    FAILED,
-    CANCELLED
+    PREPARING_DATA(false),
+    TRAINING(false),
+    PREDICTING(false),
+    FINISHED(true),
+    FAILED(true),
+    CANCELLED(true);
+
+    private final boolean isEndState;
+
+    RunnerStatus(boolean isEndState)
+    {
+        this.isEndState = isEndState;
+    }
+
+
+    public boolean isEndState()
+    {
+        return isEndState;
+    }
 }
