935fd6bf28a7b7c1adbe6a3d12ffa2d1e45dcf4b
JanSvoboda6 <jansvoboda596@gmail.com>
2022-04-05 10:37:17 +0200

    Revert "Fixing PrivateRoute condition"
    This reverts commit e3e7dcc7142a04f344f5ec49694c69d7ce85066b.

--- react/ml-runner/src/components/dataset/Datasets.tsx
+++ react/ml-runner/src/components/dataset/Datasets.tsx
@@ -9,8 +9,6 @@ import DatasetService from './DatasetService';
 import loadingAnimation from "../../styles/loading_graphics.gif";
 import FadeIn from 'react-fade-in';
 import DatasetUtility from "./DatasetUtility";
-// import { DndProvider } from 'react-dnd'
-// import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function Datasets(props)
 {
@@ -50,7 +48,6 @@ function Datasets(props)
 
     const handleCreateFolder = (key: string) =>
     {
-        console.log(key);
         setLoaded(false);
         const folder: FileInformation = {key: key};
         setFiles(folders => [...folders, folder]);
@@ -59,8 +56,6 @@ function Datasets(props)
 
     const handleCreateFiles = (addedFiles: File[], prefix: string) =>
     {
-        console.log(prefix);
-        console.log(addedFiles);
         const uniqueAddedFiles: FileInformation[] = DatasetUtility.getUniqueAddedFiles(files, addedFiles, prefix);
         DatasetService.uploadFiles(uniqueAddedFiles).then(() => setFiles(existingFiles => [...existingFiles, ...uniqueAddedFiles]));
     }
@@ -132,7 +127,6 @@ function Datasets(props)
         <div>
             <FadeIn>
                 <div className="file-editor-wrapper">
-                    {/*<DndProvider backend={HTML5Backend}>*/}
                     <FileBrowser
                         files={files.map(file =>
                         {
@@ -164,7 +158,6 @@ function Datasets(props)
                         onDownloadFile={handleDownloadFile}
                         onDownloadFolder={handleDownloadFolder}
                     />
-                    {/*</DndProvider>*/}
                 </div>
             </FadeIn>
         </div>
