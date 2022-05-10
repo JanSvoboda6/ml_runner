a913460cd8b96d24766f6d2ad851704a2f73ae24
JanSvoboda6 <jansvoboda596@gmail.com>
2022-05-10 19:45:40 +0200

    Kernel hyperParameter added to SVM

--- react/ml-runner/src/components/project/RunnerForm.tsx
+++ react/ml-runner/src/components/project/RunnerForm.tsx
@@ -6,6 +6,7 @@ function RunnerForm(props: any)
 {
     const [gammaParameter, setGammaParameter] = useState(0);
     const [cParameter, setCParameter] = useState(0);
+    const [kernelParameter, setKernelParameter] = useState("");
 
     const handleGammaParameterChange = (e: any) =>
     {
@@ -17,6 +18,12 @@ function RunnerForm(props: any)
     {
         e.preventDefault();
         setCParameter(e.target.value);
+    }
+
+    const handleKernelParameterChange = (e: any) =>
+    {
+        e.preventDefault();
+        setKernelParameter(e.target.value);
     }
 
     const handleRunButton = (e: any) =>
@@ -32,7 +39,12 @@ function RunnerForm(props: any)
             value: cParameter.toString()
         }
 
-        RunnerService.run(props.projectId, [gamma, c]);
+        const kernel: HyperParameter = {
+            name: 'kernel',
+            value: kernelParameter
+        }
+
+        RunnerService.run(props.projectId, [gamma, c, kernel]);
         window.location.reload();
     }
 
@@ -41,6 +53,7 @@ function RunnerForm(props: any)
             <div className="runner-form-block"> <h2>{props.projectName} </h2></div>
             <div className="runner-form-block"> <input type="text" onChange={handleGammaParameterChange} placeholder="Gamma parameter" /> </div>
             <div className="runner-form-block"> <input type="text" onChange={handleCParameterChange} placeholder="C parameter" /> </div>
+            <div className="runner-form-block"> <input type="text" onChange={handleKernelParameterChange} placeholder="Kernel" /> </div>
             <button className="runner-form-run-button" onClick={(e) => { handleRunButton(e); props.handleRunButton() }}>Save &amp; Run</button>
         </div>
     )
