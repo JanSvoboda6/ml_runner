10fb715ae67794a8be5d25e41b23430fa58c8048
JanSvoboda6 <jansvoboda596@gmail.com>
2022-05-14 19:51:49 +0200

    Configuration of containers

--- java/web/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO role(name) VALUES('ROLE_USER');
-INSERT INTO role(name) VALUES('ROLE_ADMIN');
-INSERT INTO user(username, password) VALUES ('jan@jan.com', '$2a$10$1SIUaUNKlkqshtozaZWbaezYNB.2jQlbFZNfTlBXqnVvzBqaAE1xC');
-INSERT INTO user_role(user_id, role_id) VALUES (1, 1);
