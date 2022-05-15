1a1de18e9b3d009536c9e5fe2920a78290e5c752
JanSvoboda6 <jansvoboda596@gmail.com>
2022-05-15 12:35:04 +0200

    Added JWT expiration assertion to PrivateRoute

--- react/ml-runner/src/helpers/PrivateRoute.jsx
+++ react/ml-runner/src/helpers/PrivateRoute.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import jwtDecode from "jwt-decode";
+import {useDispatch} from "react-redux";
 import { Route, Redirect } from 'react-router-dom';
+import LogoutService from "../services/LogoutService";
 
 const PrivateRoute = ({component: Component, ...rest}) => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        logoutUserIfAccessTokenIsExpired();
+    });
+
+    const logoutUserIfAccessTokenIsExpired = () => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user)
+        {
+            const {exp} = jwtDecode(user.accessToken);
+            if (Date.now() > exp * 1000)
+            {
+                LogoutService.logout(dispatch);
+            }
+        }
+    }
+
     return (
         <Route {...rest} render={props => (
-            localStorage.user ? <Component {...props} /> : <Component {...props} />
+            localStorage.user ? <Component {...props} /> : <Redirect to="/login" />
         )} />
     );
 };
