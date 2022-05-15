6d219b3319300334b19916711aca3bbd42192ae7
JanSvoboda6 <jansvoboda596@gmail.com>
2022-05-15 12:00:57 +0200

    Rewriting configuration base on environment

--- java/web/src/main/resources/import.sql
+++ java/web/src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO role(name) VALUES('ROLE_USER');
 INSERT INTO role(name) VALUES('ROLE_ADMIN');
-INSERT INTO user(username, password) VALUES ('jan@jan.com', '$2a$10$1SIUaUNKlkqshtozaZWbaezYNB.2jQlbFZNfTlBXqnVvzBqaAE1xC');
+INSERT INTO user(username, password, is_verified) VALUES ('jan@jan.com', '$2a$10$s9/DTr1gVXyYUJnMaCxzCujByJNTotvwEhsj0Ho9Y2mJF9qYykzsG', true);
 INSERT INTO user_role(user_id, role_id) VALUES (1, 1);
