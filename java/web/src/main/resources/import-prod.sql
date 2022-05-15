4644cae090ba1bda9fd91155b8707f04102faaf4
JanSvoboda6 <jansvoboda596@gmail.com>
2022-05-15 14:48:23 +0200

    Fixing duplicate insertion

--- java/web/src/main/resources/import-prod.sql
+++ java/web/src/main/resources/import-prod.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO role(id, name) VALUES(1, 'ROLE_USER');
 INSERT IGNORE INTO role(id, name) VALUES(2, 'ROLE_ADMIN');
-INSERT INTO user(username, password, is_verified) VALUES ('jan@jan.com', '$2a$10$s9/DTr1gVXyYUJnMaCxzCujByJNTotvwEhsj0Ho9Y2mJF9qYykzsG', true);
-INSERT INTO user_role(user_id, role_id) VALUES (1, 1);
+INSERT IGNORE INTO user(username, password, is_verified) VALUES ('jan@jan.com', '$2a$10$s9/DTr1gVXyYUJnMaCxzCujByJNTotvwEhsj0Ho9Y2mJF9qYykzsG', true);
+INSERT IGNORE INTO user_role(user_id, role_id) VALUES (1, 1);
