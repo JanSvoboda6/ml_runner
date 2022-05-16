1820332c4000f0e06b5b099f3e6c1f944eebbec2
JanSvoboda6 <jansvoboda596@gmail.com>
2022-05-16 12:06:46 +0200

    Adding environment variables for database initialization

--- database/script/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE machine_learning_database;
-CREATE USER 'springuser'@'%' IDENTIFIED BY 'ThePassword';
-GRANT ALL ON machine_learning_database.* TO 'springuser'@'%';
-USE machine_learning_database
