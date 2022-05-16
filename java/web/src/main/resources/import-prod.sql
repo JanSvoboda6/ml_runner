5b5ae9e71beab5ad69b63773e5bff39ec04fac2d
JanSvoboda6 <jansvoboda596@gmail.com>
2022-05-16 12:49:56 +0200

    Adding preconfigured account

--- java/web/src/main/resources/import-prod.sql
+++ java/web/src/main/resources/import-prod.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO role(id, name) VALUES(1, 'ROLE_USER');
 INSERT IGNORE INTO role(id, name) VALUES(2, 'ROLE_ADMIN');
-INSERT IGNORE INTO user(username, password, is_verified) VALUES ('jan@jan.com', '$2a$10$s9/DTr1gVXyYUJnMaCxzCujByJNTotvwEhsj0Ho9Y2mJF9qYykzsG', true);
+INSERT IGNORE INTO user(username, password, is_verified) VALUES ('jan@jan.com', '$2a$10$8nHq5vTlNUiAtsMkoo1TTuJPb6Q2lvrO35tgVVbO9ALkFq42gNp52', true);
 INSERT IGNORE INTO user_role(user_id, role_id) VALUES (1, 1);
